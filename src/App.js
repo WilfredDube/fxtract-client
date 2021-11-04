c12a2b6688497ccaff985a3cf84d93ebfd18c599
unbusy <unbusy@lhost.com>
2021-11-04 14:46:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { styled } from "@mui/material/styles";
+
+import "./App.css";
+import NavBar from "./components/common/NavBar";
+
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Offset />
+      <header className="App-header">Something</header>
     </div>
   );
 }
